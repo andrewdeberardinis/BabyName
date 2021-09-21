929c7723d2a2a393eb88dfec2433da7749cb2e18
andrew.deberardinis <andrew.deberardinis@MU-C1-06.mullen.pvt.k12.co.us>
2021-09-21 11:27:59 -0600

    BabyMaker project

---
+++ Baby Maker Project/src/Baby.java
@@ -0,0 +1,22 @@
+
+public class Baby
+	{
+
+	String name;
+	double weight;
+	boolean isGirl;
+	
+	public Baby(String n,double w, boolean g)
+		{
+		name = n;
+		weight = w;
+		isGirl = g;
+	
+		}
+	
+	public static void play()
+	{
+		System.out.println("Siuuuuuuuuu");
+	}
+	
+}
---
+++ Baby Maker Project/src/BabyMaker.java
@@ -0,0 +1,24 @@
+
+public class BabyMaker
+	{
+
+		public static void main(String[] args)
+			{
+			
+				Baby firstBorn = new Baby ("Mosla", 8.0, false);
+				Baby secondBorn = new Baby ("Supasadio", 7.9, false);
+				
+				System.out.println("My first baby was " + firstBorn.name);
+				System.out.println("My second baby was " + secondBorn.name);
+				
+				firstBorn.play();
+
+			
+			
+			}
+
+	
+	
+	
+	
+	}
